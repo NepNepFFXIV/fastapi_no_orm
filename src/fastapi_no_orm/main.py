1256bf270b0daeeca27179c4da7773bd6d1d6abe
Nep Nep <compsci88@gmail.com>
2025-08-29 16:12:42 -0700

    setting fastapi app

--- src/fastapi_no_orm/main.py
+++ src/fastapi_no_orm/main.py
@@ -1,8 +1,26 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
+    yield
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+def create_app() -> FastAPI:
+    from src.fastapi_no_orm.api.router import router
+
+    app = FastAPI(
+        lifespan=lifespan,
+        default_response_class=ORJSONResponse,
+    )
+
+    app.include_router(router)
+
+    return app
+
+
+app = create_app()
