5e0a92cdb560f8fb20e7732a6f94ee4c638b8ec0
Nep Nep <compsci88@gmail.com>
2025-08-30 09:39:41 -0700

    setup service

--- src/fastapi_no_orm/api/repository.py
+++ src/fastapi_no_orm/api/repository.py
@@ -4,3 +4,9 @@ from src.fastapi_no_orm.database import Postgres
 class Repository:
     def __init__(self, postgres: Postgres):
         self.postgres = postgres
+
+    async def select_operation(self, product_id: int):
+        return
+
+    async def insert_operation(self) -> None:
+        return
